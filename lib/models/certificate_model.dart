0b4bf2b1ae4ff5af290f997e32aebfe800738f95
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-03 01:45:04 +0630

    Create certificate_model.dart

---
+++ lib/models/certificate_model.dart
@@ -0,0 +1,15 @@
+class CertificateModel {
+  final String title;
+  final String issuer;
+  final String year;
+  final String badge;
+  final String url;
+
+  const CertificateModel({
+    required this.title,
+    required this.issuer,
+    required this.year,
+    required this.badge,
+    required this.url,
+  });
+}
