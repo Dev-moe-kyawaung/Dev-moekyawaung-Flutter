66f977a1ec9682ccad1444bb75183bc01ac00793
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-03 01:44:41 +0630

    Create skill_model.dart

---
+++ lib/models/skill_model.dart
@@ -0,0 +1,13 @@
+class SkillModel {
+  final String name;
+  final int percent;
+  final String category;
+  final String icon;
+
+  const SkillModel({
+    required this.name,
+    required this.percent,
+    required this.category,
+    required this.icon,
+  });
+}
