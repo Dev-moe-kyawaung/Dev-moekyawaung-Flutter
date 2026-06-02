972eea0ee1bc1a6115f68e259eba96b437ef5042
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-03 01:46:04 +0630

    Create social_links.dart

---
+++ lib/data/social_links.dart
@@ -0,0 +1,7 @@
+class SocialLinks {
+  static const github = 'https://github.com/Dev-moe-kyawaung';
+  static const linkedin = 'https://www.linkedin.com/in/moe-kyaw-aung-2653093a1';
+  static const telegram = 'https://t.me/moekyawaung';
+  static const youtube = 'https://www.youtube.com/channel/UCuTXUguZb4xjeL2nX8WJG';
+  static const email = 'moekyawaung@programmer.net';
+}
