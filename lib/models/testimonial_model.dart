efca6e629b6f4e40d624571f926160f076179c2b
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-03 01:45:26 +0630

    Create TestimonialModel class with properties

---
+++ lib/models/testimonial_model.dart
@@ -0,0 +1,13 @@
+class TestimonialModel {
+  final String name;
+  final String role;
+  final String quote;
+  final String avatar;
+
+  const TestimonialModel({
+    required this.name,
+    required this.role,
+    required this.quote,
+    required this.avatar,
+  });
+}
